b505f5b725df5e6cf14da1f5fd121039fef4acf1
Jake Ruesink <jaruesink@gmail.com>
2014-11-21 16:34:10 -0600

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testAppApp
+ * @description
+ * # testAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/main');
+    $stateProvider
+      .state('main', {
+        url: '/main',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      });
+  });
